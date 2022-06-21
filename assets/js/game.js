ec570c51bde1a049d20a07b24a5aaca6a8cefca2
ScoBoFoSho <ferrecisco@icloud.com>
2022-06-21 17:43:25 -0400

    Console Logs and Variable Names added

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,18 @@
-window.alert("This is an alert! JavaScript is running!");
+// this is the variable that allows a User to input data (i.e. their Robot's name)
+var playerName = window.prompt("What is your robot's name?");
+
+// this displays the input from the User so that the variable can be displayed
+console.log(playerName);
+
+console.log("This logs a string, good for leaving yourself a message")
+
+console.log(10+10);
+
+console.log("Our robot's name is " + playerName);
+
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+
+//fight();
